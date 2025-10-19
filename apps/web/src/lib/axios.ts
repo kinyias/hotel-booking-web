5ac00ff00137f0effae127ddcc33ea679b913eb1
Thai Tin Khang <kinyiasgit@gmail.com>
2025-10-19 18:30:29 +0700

    chore: handle network error

--- apps/web/src/lib/axios.ts
+++ apps/web/src/lib/axios.ts
@@ -33,17 +33,30 @@ api.interceptors.response.use(
   async (error) => {
     const originalRequest = error.config;
     const requestUrl = originalRequest.url;
-    const isAuthEndpoint = AUTH_WHITELIST.some((url) => requestUrl?.includes(url));
-    if (error.response?.status === 401 && !isAuthEndpoint && !originalRequest._retry) {
+    const isAuthEndpoint = AUTH_WHITELIST.some((url) =>
+      requestUrl?.includes(url)
+    );
+    // Nếu lỗi là "Network Error"
+    if (error.code === 'ERR_NETWORK') {
+      console.error('Network error — cannot reach API server.');
+      alert('Network error — cannot reach server.');
+      return Promise.reject(error);
+    }
+    if (
+      error.response?.status === 401 &&
+      !isAuthEndpoint &&
+      !originalRequest._retry
+    ) {
       originalRequest._retry = true;
-      
+
       try {
-        
-        const { data } = await axios.post(`${API_BASE_URL}${API_ENDPOINTS.AUTH.REFRESH_TOKEN}`);
-        
+        const { data } = await api.post(
+          `${API_BASE_URL}${API_ENDPOINTS.AUTH.REFRESH_TOKEN}`
+        );
+
         // Save the new tokens
         setAuthTokens(data.accessToken);
-        
+
         // Retry the original request
         originalRequest.headers.Authorization = `Bearer ${data.accessToken}`;
         return axios(originalRequest);
@@ -54,17 +67,20 @@ api.interceptors.response.use(
         return Promise.reject(refreshError);
       }
     }
-    
+
     return Promise.reject(error);
   }
 );
 
 // Helper function to set auth tokens
 export const setAuthTokens = (accessToken: string) => {
-  
   // Store tokens in cookies
-  Cookies.set('accessToken', accessToken, {expires: 7, secure: true, sameSite: 'strict' });
-  
+  Cookies.set('accessToken', accessToken, {
+    expires: 7,
+    secure: true,
+    sameSite: 'strict',
+  });
+
   // Update axios headers
   api.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
 };
