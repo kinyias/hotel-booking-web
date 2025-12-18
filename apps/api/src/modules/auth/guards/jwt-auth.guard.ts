c635e2c5b4935245059068d72616e57a1ad7154b
Duong Nhat Thien <dangkhoa2k30@gmail.com>
2025-12-18 13:07:54 +0700

    feat: add public decorate

--- apps/api/src/modules/auth/guards/jwt-auth.guard.ts
+++ apps/api/src/modules/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,25 @@
+// auth/guards/jwt-auth.guard.ts
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { IS_PUBLIC_KEY } from 'src/common/decorators/public.decorator';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true; // ⛔ bypass passport-jwt
+    }
+
+    return super.canActivate(context);
+  }
+}
